1373447357211d9ef094ccb17b1b00482e3a0ffe
devmmad <mohammad.ali.asheghi@gmail.com>
2026-04-25 10:21:26 +0330

    Initial Commit

---
+++ aily/src/test/java/com/vertex/aily/AilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vertex.aily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AilyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
